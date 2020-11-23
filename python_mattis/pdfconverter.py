3f7d501a1a43c2bbab3c991c4da25327d070ca50
Julia Prein <jprein@me.com>
2020-11-23 12:02:34 +0100

    clean up files

---
+++ python_mattis/pdfconverter.py
@@ -0,0 +1,46 @@
+# https://stackoverflow.com/questions/26494211/extracting-text-from-a-pdf-file-using-pdfminer-in-python
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+from os import path
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+if __name__ == '__main__':
+    from sys import argv
+    # print(convert_pdf_to_txt(argv[1]))
+    text_file = open("test.txt", "w")
+    n = text_file.write(convert_pdf_to_txt(argv[1]))
+    text_file.close()
+
+# TODO: durch directory gehen, alle PDFs listen, einlesen in LIste. 
+
+# import glob, os und io funktionieren nicht. 
+# import glob
+# glob.glob("*.pdf")
+
+# files = [f for f in os.listdir('.') if os.path.isfile(f)]
+# files = filter(lambda f: f.endswith(('.pdf','.PDF')), files)
